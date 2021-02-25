ec932840369271d5aa46016f0f2d15a8cae4266e
Awadhesh Chaurasia <awadhesh.chaurasia@mediassist.in>
2021-02-25 13:38:28 +0530

    changes

--- src/utils/customPipeUtil.ts
+++ src/utils/customPipeUtil.ts
@@ -1,9 +1,9 @@
-// will return string with 1st letter in caps
+// Will return string with 1st letter in caps
 export const toTitleCase = (str: string): string => {
   return str.charAt(0).toUpperCase() + str.substring(1, str.length).toLowerCase();
 };
 
 // Will return a sentence with making 1st letter of each word in Caps
 export const toSentenceCase = (str: string): string => {
-    return str.toLowerCase().replace(/(^\w{1})|(\s{1}\w{1})/g, (match: any): any => match.toUpperCase());
+  return str.toLowerCase().replace(/(^\w{1})|(\s{1}\w{1})/g, (match: any): any => match.toUpperCase());
 };
